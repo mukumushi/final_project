7a280a5c682ddb93652ab015c561ae92f7cc3542
trish <trish.willard@gmail.com>
2020-08-26 13:08:56 -0400

    completed basic website

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+
+window.addEventListener('load', function(){
+  document.querySelector('.intro').classList.add('intro-open');
+})
+
---
+++ js/projects.js
@@ -0,0 +1,14 @@
+let projects = [
+    {
+        title: "Shoes",
+        src: "images/shoes.png",
+        price: 74.99,
+        onSale: false
+    },
+    {
+        title: "Hat",
+        src: "images/hat.jpg",
+        price: 24.99,
+        onSale: false
+    },
+]
