d2ccf31828cfe6dc24c3632d2b897abcaa5cd971
trishwillard <trish@visionistinc.com>
2020-09-01 00:02:43 -0400

    Added a lot of styles for hovering

--- js/app.js
+++ js/app.js
@@ -1,36 +1,51 @@
 let projects = [
   {
-      type: "kids",
-      title: 'poster 1',
+      type: "Home",
+      title: 'Hello Beautiful',
       href: "projects/quitstart.html",
-      desc: 'a description goes here'
+      desc: 'a description goes here',
+      url: '../images/projectcovers/3.jpg'
   },
   {
-      type: "kids",
+      type: "Kitchen",
       title: 'poster 2',
       href: "projects/2.html",
-      desc: 'anoter description goes here'
+      desc: 'another description goes here',
+      url: '../images/projectcovers/2.jpg'
   },
+  {
+     type: "Kids",
+     title: 'poster 2',
+     href: "projects/2.html",
+     desc: 'just a short 1 liner',
+     url: '../images/projectcovers/4.jpg'
+},
+  {
+      type: "Kids",
+      title: 'poster 2',
+      href: "projects/2.html",
+      desc: 'another description goes here',
+      url: '../images/projectcovers/1.jpg'
+},
 ]
+
+// this controls the splash screen
 
 window.addEventListener('load', function(){
   document.querySelector('.intro').classList.add('intro-open');
 })
 
 
-
-
-
 for(let i = 0; i < projects.length; i++) {
   
   let cardDiv = document.createElement('div')
   cardDiv.classList.add('card')
-
-  document.querySelector('#cardsgohere').appendChild(cardDiv)
+  cardDiv.style.backgroundImage = "url(" + projects[i].url + ")"
+  document.getElementById('portfolio').appendChild(cardDiv)
 
   let contentDiv = document.createElement('div')
-  contentDiv.classList.add('card-content')
-
+  contentDiv.classList.add('card-content', 'hidden')
+ 
   cardDiv.appendChild(contentDiv)
 
   let h3 = document.createElement('h3')
@@ -49,5 +64,11 @@ for(let i = 0; i < projects.length; i++) {
   button.innerText = "see more"
   contentDiv.appendChild(button)
 
+  cardDiv.addEventListener('mouseover', () => {
+    contentDiv.classList.remove('hidden')
+  })
+  cardDiv.addEventListener('mouseleave', () => {
+    contentDiv.classList.add('hidden')
+  })
  
 }
